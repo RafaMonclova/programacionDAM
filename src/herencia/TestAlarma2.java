06334856aa92d29af44d27e01c3427bdc9a9cfc7
rafa <rafa@DESKTOP-SUVCMUG>
2022-02-24 13:24:00 +0100

    Tarea Alarma 24/02

--- src/herencia/TestAlarma2.java
+++ src/herencia/TestAlarma2.java
@@ -1,21 +1,35 @@
 
 package herencia;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.Timer;
 /**
  *
  * @author rafa
  */
 public class TestAlarma2 {
+    
+    //temporizador
+    public static void delaySegundos(){
+        
+        try{
+            Thread.sleep(1300);
+        }catch(InterruptedException e){};
+        
+    }
+    
     public static void main(String[] args){
         
-        
+       
         RelojAlarma2 miAlarma2 = new RelojAlarma2();
         
         miAlarma2.setTime(18, 35, 23);
         miAlarma2.setAlarm(18, 52);
         
-        int incremento = 1000;
+        int incremento = 10;
         while(incremento > 0){
+            
+            delaySegundos();
             miAlarma2.tictac();
             incremento--;
             miAlarma2.display();
