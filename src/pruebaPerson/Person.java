8babf241811db36a28de5f8e5b42ead8782722dd
alumno <alumno@pc06-t1.inf>
2022-03-07 17:54:15 +0100

    Clase 7/03-Person empezado

--- src/pruebaPerson/Person.java
+++ src/pruebaPerson/Person.java
@@ -9,6 +9,25 @@ package pruebaPerson;
  *
  * @author alumno
  */
-public class Person {
+public class Person implements PersonInterface{
+
+    
+    protected int cashSaving;
+    protected int retirementFund;
+    protected String firstName;
+    protected String lastName;
+    
+    
+    @Override
+    public int computeTotalWealth() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
     
 }
