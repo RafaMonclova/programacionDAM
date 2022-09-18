198493e7eee4df927d4244121f5e8a4ac5ed9efc
rafa <rafa@DESKTOP-3AVVSRA.home>
2022-09-18 17:20:03 +0200

    Prueba

---
+++ src/github/cosa.java
@@ -0,0 +1,18 @@
+
+package github;
+
+/**
+ *
+ * @author rafa
+ */
+public class cosa {
+    
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Holiwi");
+        
+    }
+    
+    
+}
